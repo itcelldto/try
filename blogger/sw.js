0fc9c713b25e0af8676852862e8d2f5ebfe32451
itcelldto <itcell.dto@gmail.com>
2025-06-20 21:35:27 +0530

    Create sw.js

---
+++ blogger/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'blogger-cache-v1';
+const urlsToCache = [
+  '/try/blogger/',
+  '/try/blogger/index.html',
+  '/try/blogger/styles.css',
+  '/try/blogger/assets/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
